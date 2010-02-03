522aeee0721d1876ecc347ffaf0119a40a52554e
Portal Guerrero <portal@guerrero.gob.mx>
2010-02-02 20:35:45 -0600

    Se eliminan archivos Thumb.db e imagenes no usadas

--- Portal_files/ajs_003.php
+++
@@ -1,7 +0,0 @@
-var OX_7332ed05 = '';
-OX_7332ed05 += "<"+"object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0\" width=\"410\" height=\"111\">\n";
-OX_7332ed05 += "<"+"param name=\"movie\" value=\"http://www.guerrero.gob.mx/art_portal/banner-index.swf\">\n";
-OX_7332ed05 += "<"+"param name=\"quality\" value=\"high\">\n";
-OX_7332ed05 += "<"+"embed src=\"http://www.guerrero.gob.mx/art_portal/banner-convoca.swf\" quality=\"high\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\" type=\"application/x-shockwave-flash\" width=\"415\" height=\"111\"><"+"/embed>\n";
-OX_7332ed05 += "<"+"/object><"+"div id=\'beacon_5ced90a08e\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'http://www.guerrero.gob.mx/banners/www/delivery/lg.php?bannerid=24&amp;campaignid=3&amp;zoneid=9&amp;channel_ids=,&amp;loc=http%3A%2F%2Fwww.guerrero.gob.mx%2F%3FV%3DCPI2010&amp;cb=5ced90a08e\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
-document.write(OX_7332ed05);
